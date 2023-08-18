986eb349a23515b221034700f9fed78e88dee87c
Bonaventure F. P. Dossou <femipancrace.dossou@gmail.com>
2023-08-17 22:22:08 -0400

    Removed wandb, added all language data, updated training and evaluation code

--- README.md
+++ README.md
@@ -1,21 +1,18 @@
-Toward Building a Multi-Task Learning Model for Fon Language
-
-# Structure
-- Get Task and Data
-    - NER and POS already added
-    - We can add other tasks (there is QA dataset for Fon in AfriQA but I am not familiar with QA downstream task)
+Toward Building a Multi-Task Learning Model for Fon Language, accepted at WiNLP@EMNLP2023
 
 - Build Multi-task Learning Model
-    - I found [this](https://towardsdatascience.com/multi-task-learning-with-pytorch-and-fastai-6d10dc7ce855?gi=362f8e810585 ) to be a good tutorial and walk-through
-    - For the shared layers (encoder) we can use any existing pretrained Afro-Centric LM
-        - AfroLM (by me): https://huggingface.co/bonadossou/afrolm_active_learning
-        - Afriberta: https://huggingface.co/castorini/afriberta_large (to be integrated if we decide to include it too)
 
-    - For now the code is built using AfroLM:
-        - `@inproceedings{dossou-etal-2022-afrolm, title = "{A}fro{LM}: A Self-Active Learning-based Multilingual Pretrained Language Model for 23 {A}frican Languages", author = "Dossou, Bonaventure F. P. and Tonja, Atnafu Lambebo and Yousuf, Oreen and Osei, Salomey and Oppong, Abigail and Shode, Iyanuoluwa and Awoyomi, Oluwabusayo Olufunke and Emezue, Chris", booktitle = "Proceedings of The Third Workshop on Simple and Efficient Natural Language Processing (SustaiNLP)", month = dec, year = "2022", address = "Abu Dhabi, United Arab Emirates (Hybrid)", publisher = "Association for Computational Linguistics", url = "https://aclanthology.org/2022.sustainlp-1.11", pages = "52--64"}`
+    - For the shared layers (encoder) we can use any existing pretrained Afro-Centric LM
+
+        - AfroLM-Large: https://huggingface.co/bonadossou/afrolm_active_learning
+            * `@inproceedings{dossou-etal-2022-afrolm, title = "{A}fro{LM}: A Self-Active Learning-based Multilingual Pretrained Language Model for 23 {A}frican Languages", author = "Dossou, Bonaventure F. P. and Tonja, Atnafu Lambebo and Yousuf, Oreen and Osei, Salomey and Oppong, Abigail and Shode, Iyanuoluwa and Awoyomi, Oluwabusayo Olufunke and Emezue, Chris", booktitle = "Proceedings of The Third Workshop on Simple and Efficient Natural Language Processing (SustaiNLP)", month = dec, year = "2022", address = "Abu Dhabi, United Arab Emirates (Hybrid)", publisher = "Association for Computational Linguistics", url = "https://aclanthology.org/2022.sustainlp-1.11", pages = "52--64"}`
+        
+        - XLMR-Large: https://huggingface.co/xlm-roberta-large
+            * `@inproceedings{conneau-etal-2020-unsupervised, title = "Unsupervised Cross-lingual Representation Learning at Scale", author = "Conneau, Alexis  and Khandelwal, Kartikay and Goyal, Naman  and Chaudhary, Vishrav and Wenzek, Guillaume  and Guzm{\'a}n, Francisco and Grave, Edouard and Ott, Myle and Zettlemoyer, Luke and Stoyanov, Veselin", booktitle = "Proceedings of the 58th Annual Meeting of the Association for Computational Linguistics", month = jul, year = "2020", address = "Online", publisher = "Association for Computational Linguistics", url = "https://aclanthology.org/2020.acl-main.747", doi = "10.18653/v1/2020.acl-main.747", pages = "8440--8451"}`
 
 - Evaluation:
-    - The goal primarily is to detect whether multitask learning improves performance on downstream tasks for Fon. We will evaluate the multi-task learning model on the task defined above, and a baseline would be to see how the models would perform when created for individual tasks (I have results for NER (for previous works) for Afriberta and AfroLM)
+
+    - The goal primarily is to detect whether multitask learning improves performance on downstream tasks for Fon. We try two settings: (a) training only on Fon and evaluating on Fon, (b) training on all languages and evaluating on Fon. We evaluate the multi-task learning model on NER and POS tasks, and compare it with baselines (models finetuned and evaluated on single tasks)
 
 # How to get started
 
@@ -27,9 +24,3 @@ This command will:
 - Install required libraries: `pip install -r requirements.txt -q`
 - Move to the code folder: `cd code`
 - Run the training: `python run_train.py`
-
-
-# To be done
-
-- Testing code (very similar to validation) - easily done if training successful
-- ...
--- code/run_train.py
+++ code/run_train.py
@@ -2,6 +2,7 @@ import os
 from utils import *
 import torch
 from torch.nn import CrossEntropyLoss
+from seqeval.metrics import f1_score, precision_score, recall_score, classification_report
 from torch.utils.data import DataLoader
 from transformers import XLMRobertaTokenizer, XLMRobertaModel, XLMRobertaForMaskedLM, AutoTokenizer, AutoModel
 
@@ -22,9 +23,14 @@ class MultitaskFON:
 
         self.labels_ner_path = args.labels_ner_path
         self.labels_pos_path = args.labels_pos_path
-        self.ner_data = "../data/ner/fon/"
-        self.pos_data = "../data/pos/fon/"
-
+        
+        if args.fon_only:
+            self.ner_data = "../data/ner/fon/"
+            self.pos_data = "../data/pos/fon/"
+        else:
+            self.ner_data = "../data/ner/all/"
+            self.pos_data = "../data/pos/all/"
+        
         self.labels_ner = get_ner_labels(self.labels_ner_path)
         self.labels_pos = get_pos_labels(self.labels_pos_path)
 
@@ -47,21 +53,29 @@ class MultitaskFON:
         self.train_dataset_pos = load_pos_examples(self.pos_data, self.tokenizer_pos, self.labels_pos, self.pad_token_label_id, mode="train")
 
         self.dev_dataset_ner = load_ner_examples(self.ner_data, self.tokenizer_ner, self.labels_ner, self.pad_token_label_id, mode="dev")
-        self.dev_dataset_pos = load_pos_examples(self.pos_data, self.tokenizer_pos, self.labels_pos, self.pad_token_label_id, mode="dev") #same tokenizer as NER
+        self.dev_dataset_pos = load_pos_examples(self.pos_data, self.tokenizer_pos, self.labels_pos, self.pad_token_label_id, mode="dev")
+
+        self.test_dataset_ner = load_ner_examples(self.ner_data, self.tokenizer_ner, self.labels_ner, self.pad_token_label_id, mode="test")
+        self.test_dataset_pos = load_pos_examples(self.pos_data, self.tokenizer_pos, self.labels_pos, self.pad_token_label_id, mode="test")
 
         self.train_dataset = [self.train_dataset_ner, self.train_dataset_pos]
         self.dev_dataset = [self.dev_dataset_ner, self.dev_dataset_pos]
+        self.test_dataset = [self.test_dataset_ner, self.test_dataset_pos]
 
         self.labels = [self.labels_ner, self.labels_pos]
 
         self.train_batch_size = args.train_batch_size
         self.dev_batch_size = args.dev_batch_size
+        self.test_batch_size = args.test_batch_size
 
         self.train_dataloader_ner = DataLoader(self.train_dataset[0], batch_size=self.train_batch_size, shuffle=True)
         self.train_dataloader_pos = DataLoader(self.train_dataset[1], shuffle=True, batch_size=self.train_batch_size)
 
         self.dev_dataloader_ner = DataLoader(self.dev_dataset[0], batch_size=self.dev_batch_size, shuffle=False)
         self.dev_dataloader_pos = DataLoader(self.dev_dataset[1], shuffle=False, batch_size=self.dev_batch_size)
+
+        self.test_dataloader_ner = DataLoader(self.test_dataset[0], batch_size=self.test_batch_size, shuffle=False)
+        self.test_dataloader_pos = DataLoader(self.test_dataset[1], shuffle=False, batch_size=self.test_batch_size)
 
         # define the model
         seq_length_ner, seq_length_pos = 0, 0
@@ -70,6 +84,9 @@ class MultitaskFON:
             seq_length_ner = ner_batch[0].shape[-1]
             seq_length_pos = pos_batch[0].shape[-1]
             break#####################################
+        
+        self.seq_length_ner = seq_length_ner
+        self.seq_length_pos = seq_length_pos
 
         self.model = MultiTaskModel(self.encoders, [self.num_labels_ner, self.num_labels_pos], [seq_length_ner, seq_length_pos])
         self.model = to_device(self.model, self.num_gpus, self.device)
@@ -171,20 +188,115 @@ class MultitaskFON:
 
             if epoch_dev_loss < best_dev_loss:
                 best_dev_loss = epoch_dev_loss
-                path = os.path.join('../models/multitask_model_fon.bin')
+                path = 'multitask_model_fon.bin'
                 torch.save(self.model.state_dict(), path)
 
             print("Epoch {}'s validation loss: {}".format(epoch + 1, epoch_dev_loss))
+        print('Best validation loss: {}'.format(best_dev_loss))
 
-            print('Best validation loss: {}'.format(best_dev_loss))
+    def test(self):         
+        print("***** Running testing *****")
+        print("Num examples NER = %d", len(self.test_dataset[0]))
+        print("Num examples POS = %d", len(self.test_dataset[1]))
 
+        model = MultiTaskModel(self.encoders, [self.num_labels_ner, self.num_labels_pos],
+                               [self.seq_length_ner, self.seq_length_pos])
+        model = to_device(model, self.num_gpus, self.device)
+        model.load_state_dict(torch.load('multitask_model_fon.bin'))
+        model.eval()
+
+        ner_preds = None
+        ner_label_ids = None
+
+        pos_preds = None
+        pos_label_ids = None
+
+        for test_batches in zip(self.test_dataloader_ner, self.test_dataloader_pos):
+            test_ner_batch, test_pos_batch = test_batches
+
+            test_ner_batch = tuple(t.to(self.device) for t in test_ner_batch)
+            test_pos_batch = tuple(t.to(self.device) for t in test_pos_batch)
+
+            test_ner_batch = {"input_ids": test_ner_batch[0], "attention_mask": test_ner_batch[1]}
+            test_pos_batch = {"input_ids": test_pos_batch[0], "attention_mask": test_pos_batch[1]}
+
+            test_ner_batch["token_type_ids"] = test_ner_batch[2]
+            test_pos_batch["token_type_ids"] = test_pos_batch[2]
+
+            with torch.no_grad():
+                test_outputs_ner, test_outputs_pos = model(test_ner_batch, test_pos_batch)
+
+            if ner_preds is None:
+                ner_preds = test_outputs_ner.detach().cpu().numpy()
+                ner_label_ids = test_ner_batch[3].detach().cpu().numpy()
+            else:
+                ner_preds = np.append(ner_preds, test_outputs_ner.detach().cpu().numpy(), axis=0)
+                ner_label_ids = np.append(ner_label_ids, test_ner_batch[3]["labels"].detach().cpu().numpy(), axis=0)
+
+            if pos_preds is None:
+                pos_preds = test_outputs_pos.detach().cpu().numpy()
+                pos_label_ids = test_pos_batch[3].detach().cpu().numpy()
+            else:
+                pos_preds = np.append(pos_preds, test_outputs_pos.detach().cpu().numpy(), axis=0)
+                pos_label_ids = np.append(pos_label_ids, test_pos_batch[3]["labels"].detach().cpu().numpy(), axis=0)
+
+        ner_preds = np.argmax(ner_preds, axis=2)
+        pos_preds = np.argmax(pos_preds, axis=2)
+
+        ner_label_map = {i: label for i, label in enumerate(self.labels_ner)}
+        pos_label_map = {i: label for i, label in enumerate(self.labels_pos)}
+        
+        ner_label_list = [[] for _ in range(ner_label_ids.shape[0])]
+        pos_label_list = [[] for _ in range(pos_label_ids.shape[0])]
+
+        ner_preds_list = [[] for _ in range(ner_label_ids.shape[0])]
+        pos_preds_list = [[] for _ in range(pos_label_ids.shape[0])]
+
+        for i in range(ner_label_ids.shape[0]):
+            for j in range(ner_label_ids.shape[1]):
+                if ner_label_ids[i, j] != self.pad_token_label_id:
+                    ner_label_list[i].append(ner_label_map[ner_label_ids[i][j]])
+                    ner_preds_list[i].append(ner_label_map[ner_preds[i][j]])
+
+        for i in range(pos_label_ids.shape[0]):
+            for j in range(pos_label_ids.shape[1]):
+                if pos_label_ids[i, j] != self.pad_token_label_id:
+                    pos_label_list[i].append(pos_label_map[pos_label_ids[i][j]])
+                    pos_preds_list[i].append(pos_label_map[pos_preds[i][j]])
+
+        ner_results = {
+        "precision": precision_score(ner_label_list, ner_preds_list),
+        "recall": recall_score(ner_label_list, ner_preds_list),
+        "f1": f1_score(ner_label_list, ner_preds_list),
+        'report': classification_report(ner_label_list, ner_preds_list)
+        }
+
+        pos_results = {
+        "precision": precision_score(pos_label_list, pos_preds_list),
+        "recall": recall_score(pos_label_list, pos_preds_list),
+        "f1": f1_score(pos_label_list, pos_preds_list),
+        'report': classification_report(pos_label_list, pos_preds_list)
+        }
+
+        with open('mtl_fon_ner_results.txt', "w") as ner_writer:
+            for key in sorted(ner_results.keys()):
+                ner_writer.write("{} = {}\n".format(key, str(ner_results[key])))
+
+        ner_writer.close()
+
+        with open('mtl_fon_pos_results.txt', "w") as pos_writer:
+            for key in sorted(pos_results.keys()):
+                pos_writer.write("{} = {}\n".format(key, str(pos_results[key])))
+        
+        pos_writer.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Multitask FON')
     parser.add_argument('--labels_ner_path', type=str, help='file path to label file for NER task')
     parser.add_argument('--labels_pos_path', type=str, help='file path to label file for POS task')
-    parser.add_argument('--train_batch_size', type=int, default=8, help='training batch size')
-    parser.add_argument('--dev_batch_size', type=int, default=8, help='testing batch size')
+    parser.add_argument('--train_batch_size', type=int, default=4, help='training batch size')
+    parser.add_argument('--dev_batch_size', type=int, default=4, help='dev batch size')
+    parser.add_argument('--test_batch_size', type=int, default=4, help='testing batch size')
     parser.add_argument('--epochs', type=int, default=50, help='number of epochs')
     parser.add_argument('--learning_rate', type=float, default=3e-5, help='learning rate')
     parser.add_argument('--hf_model_path_ner', type=str, default="bonadossou/afrolm_active_learning", help='Hugging Face encoder model path')
@@ -192,7 +304,10 @@ if __name__ == "__main__":
     parser.add_argument('--hf_tokenizer_path_ner', type=str, default="bonadossou/afrolm_active_learning", help='Hugging Face tokenizer path')
     parser.add_argument('--hf_tokenizer_path_pos', type=str, default="xlm-roberta-large", help='Hugging Face tokenizer path')
     parser.add_argument('--dynamic_weighting', action='store_true', help='dynamic weighting')
-    args = parser.parse_args()
+    parser.add_argument('--fon_only', type=bool, default=False, help='train only on fon or on all languages data')
 
+    args = parser.parse_args()
     mt_fon = MultitaskFON(args)
+
     mt_fon.train()
+    mt_fon.test()
