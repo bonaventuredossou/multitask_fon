6f373ff9fbf0aeccada92afa75f04f56f360d675
Bonaventure F. P. Dossou <femipancrace.dossou@gmail.com>
2023-08-25 20:49:43 -0400

    Added shell file to add run model on Fon data only

---
+++ run_fon_only.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=multitaskfon_only
+#SBATCH --gres=gpu:rtx8000:8
+#SBATCH --cpus-per-gpu=8
+#SBATCH --mem=128G
+#SBATCH --time=168:00:00
+#SBATCH --partition=long
+#SBATCH --error=/home/mila/b/bonaventure.dossou/multitask_fon/slurmerror_only.txt
+#SBATCH --output=/home/mila/b/bonaventure.dossou/multitask_fon/slurmoutput_only.txt
+
+
+###########cluster information above this line
+module load python/3.9 cuda/10.2/cudnn/7.6
+source /home/mila/b/bonaventure.dossou/env/bin/activate
+cd /home/mila/b/bonaventure.dossou/multitask_fon
+pip install -r requirements.txt
+cd code
+python run_train.py --fon_only True
